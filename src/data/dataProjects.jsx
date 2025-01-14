5819388518d2cad5d8757c6c9f2e43c237ca51e9
Felipe Daniel Juarez Alvarez <spider_spainfel@hotmail.com>
2025-01-14 11:37:50 -0600

    Feat: new proyect

--- src/data/dataProjects.jsx
+++ src/data/dataProjects.jsx
@@ -1,11 +1,18 @@
 import { FaReact, FaNodeJs } from "./dataIcons";
-import { SiReactbootstrap, SiVite, SiReactrouter, SiFramer, SiGithubpages, SiMui } from "./dataIcons";
+import {
+  SiReactbootstrap,
+  SiVite,
+  SiReactrouter,
+  SiFramer,
+  SiGithubpages,
+  SiMui,
+} from "./dataIcons";
 import { GiSteamLocomotive } from "./dataIcons";
 
 export const profileData = [
   {
     titulo: "Pokedex: Tu Guía Pokémon Definitiva",
-    imagen: "/assets/poke-page.png",  // Ruta absoluta desde public
+    imagen: "/assets/poke-page.png", // Ruta absoluta desde public
     texto:
       "Pokedex es una aplicación que gestiona y analiza información detallada sobre Pokémon, ofreciendo estadísticas completas, habilidades y características, todo en una interfaz intuitiva y fácil de usar.",
     listChips: [
@@ -56,8 +63,60 @@ export const profileData = [
     linkHub: "https://github.com/felipejrz/pokemon-zeiropedia",
   },
   {
+    titulo: "Tienda: Encuentra Todo lo que Necesitas",
+    imagen: "/assets/shop-page.png", // Ruta absoluta desde public
+    texto:
+      "Nuestra tienda es la solución perfecta para descubrir y adquirir los productos que deseas. Ofrecemos una amplia variedad, desde tecnología hasta hogar, con descripciones detalladas, opiniones de clientes y ofertas exclusivas, todo en una plataforma intuitiva y fácil de usar.",
+    listChips: [
+      {
+        label: "Angular",
+        icon: <FaReact />,
+        variant: "outlined",
+        color: "#61DAFB",
+      },
+      {
+        label: "Tailwind",
+        icon: <SiReactbootstrap />,
+        variant: "outlined",
+        color: "#41E0FD",
+      },
+      {
+        label: "Node.js",
+        icon: <FaNodeJs />,
+        variant: "outlined",
+        color: "#5FA04E",
+      },
+      {
+        label: "Vite.js",
+        icon: <SiVite />,
+        variant: "outlined",
+        color: "#646CFF",
+      },
+      {
+        label: "React Router",
+        icon: <SiReactrouter />,
+        variant: "outlined",
+        color: "#CA4245",
+      },
+      {
+        label: "Framer Motion",
+        icon: <SiFramer />,
+        variant: "outlined",
+        color: "#0055FF",
+      },
+      {
+        label: "Github Pages",
+        icon: <SiGithubpages />,
+        variant: "outlined",
+        color: "#BBDDE5",
+      },
+    ],
+    linkPages: "https://felipejrz.github.io/pokemon-zeiropedia/",
+    linkHub: "https://github.com/felipejrz/signal-store",
+  },
+  {
     titulo: "Portafolio: Innovación en Desarrollo Web",
-    imagen: "/assets/profile-page.png",  // Ruta absoluta desde public
+    imagen: "/assets/profile-page.png", // Ruta absoluta desde public
     texto:
       "Un portafolio destacable que muestra proyectos innovadores y soluciones avanzadas en desarrollo web. Descubre cómo combino creatividad y tecnología para crear experiencias digitales únicas y funcionales.",
     listChips: [
