16424f89afc623c7f2ef424a5590ecd26335207a
Felipe Daniel Juarez Alvarez <spider_spainfel@hotmail.com>
2025-01-14 11:42:25 -0600

    Update: Link proyect profile web

--- src/data/dataProjects.jsx
+++ src/data/dataProjects.jsx
@@ -157,7 +157,7 @@ export const profileData = [
         color: "#d4c9c9",
       },
     ],
-    linkPages: "https://felipejrz.github.io/pokemon-zeiropedia/",
+    linkPages: "https://felipe-juarez-porfolio.netlify.app",
     linkHub: "https://github.com/felipejrz/web-profile",
   },
 ];
